4ce6cb053969346d4a8984de1a9e9365dd48fd8c
Ruslan <rchekalov@me.com>
2026-05-15 20:52:09 +0400

    version: bump to 0.6.2

--- internal/version/version.go
+++ internal/version/version.go
@@ -6,4 +6,4 @@ package version
 // Overridable at link time via:
 //
 //	go build -ldflags "-X github.com/rchekalov/silo/internal/version.Version=<tag>"
-var Version = "0.6.1"
+var Version = "0.6.2"
